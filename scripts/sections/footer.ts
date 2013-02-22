ed32b1230a6885595964961161699baddb188462
digitalgremlin <jslade@flammabletoys.com>
2013-02-22 06:02:55 -0800

    Changes I have made

--- scripts/sections/footer.ts
+++ scripts/sections/footer.ts
@@ -1,5 +1,43 @@
-# $("./body") {
-#   insert_bottom("footer", class: "_footer") {
-#     Move stuff here
-#   }
-# }
+$("./body") {
+
+	$("//footer"){
+	add_class('_footer')
+	remove(".//br")
+	$("//div[contains(concat(' ', @class, ' '), ' FooterBox1 ')]"){
+	add_class('_menuContainer')
+	}
+	$("//div[contains(concat(' ', @class, ' '), ' _menuContainer ')]"){
+	attribute("data-ur-set") {
+    value("toggler")
+   }
+	insert_top("h2", class: "_menuTitle", "Footer Menu"){
+	attribute("data-ur-toggler-component") {
+    value("button")
+   }
+   }
+     
+   wrap_together("./div", "div", data-ur-toggler-component: "content", class: "_footMenus"){
+   $("./div"){
+   attribute("data-ur-set"){
+   value("toggler")
+   }
+   $("./p[1]"){
+   attribute("data-ur-toggler-component") {
+    value("button")
+   }
+   add_class('_subFootMenu')
+   }
+
+   $("./p[2]"){
+   attribute("data-ur-toggler-component") {
+    value("content")
+   }
+   
+   }
+   }
+   }
+   }
+	
+
+	}
+  }
--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -1,5 +1,41 @@
-# $('./body') {
-#   insert_top("header", class: "_header") {
-#     Move stuff here
-#   }
-# }
+$('./body') {
+
+$('.//header'){
+	add_class('_header')
+	attribute("data-ur-set") {
+    value("toggler")
+ 	}
+	$$('#searchForm'){
+	add_class('_searchForm')	
+	}
+	insert_top('div', class: '_top'){
+	insert("div", class: "_eyeGlass", data-ur-toggler-component: "button"){
+	insert("div", class: "menu-search")
+	}
+	
+	insert_after('div', class: '_search', data-ur-toggler-component: "content"){
+	move_here("//form[contains(concat(' ', @class, ' '), ' _searchForm ')]"){
+	$$('.SearchBtn'){
+	attributes(type: "submit", value: "GO")
+ 	add_class('menu-search')
+	}
+		
+	wrap("div", class: "_searchWrapper")
+	}
+
+	}
+	}
+
+	$$('.Padding5T > a'){
+	add_class('_logo')
+	move_to("//div[contains(concat(' ', @class, ' '), ' _top ')]")
+
+	$('./img'){
+	remove()
+	}
+	insert("img", src: asset("images/logo.png"))
+	}
+
+}  
+
+}
