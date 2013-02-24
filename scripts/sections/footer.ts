dc6ff4d8594217805cdbde38ce176a56eee24ad6
digitalgremlin <jslade@flammabletoys.com>
2013-02-23 23:00:14 -0800

    Changes I have made

--- scripts/sections/footer.ts
+++ scripts/sections/footer.ts
@@ -6,6 +6,9 @@ $("./body") {
 	$("//div[contains(concat(' ', @class, ' '), ' FooterBox1 ')]"){
 	add_class('_menuContainer')
 	}
+  $("//div[contains(concat(' ', @class, ' '), ' EmailSocialBox ')]"){
+  add_class('_emailSocialBox')
+  }
 	$("//div[contains(concat(' ', @class, ' '), ' _menuContainer ')]"){
 	attribute("data-ur-set") {
     value("toggler")
@@ -14,10 +17,11 @@ $("./body") {
   insert("span", class: "menu-collapse")
 	attribute("data-ur-toggler-component") {
     value("button")
+   }   
    }
-   }
-     
+   
    wrap_together("./div", "div", data-ur-toggler-component: "content", class: "_footMenus"){
+
    $("./div"){
    attribute("data-ur-set"){
    value("toggler")
@@ -47,8 +51,25 @@ $("./body") {
    }
    }
    }
+   $("./div[contains(concat(' ', @class, ' '), ' _footMenus ')]"){
+   insert_top("div", class: "_subscribe"){
+   move_here("//div[contains(concat(' ', @class, ' '), ' _emailSocialBox ')]"){
+   $(".//b"){
+   insert_after("br")
    }
-	
+   $("./div[contains(concat(' ', @class, ' '), ' SafeSubscribe ')]"){
+   add_class('_hidden')
+   }
+   }
+   }
+   }
+   %one = this()
+   $("//div[@class='LogoCopyright']"){
+    move(this(), %one, position("after"))
+   }
+   }
+   remove("./div[contains(concat(' ', @class, ' '), ' FooterBox2 ')]")
+   remove("./div[contains(concat(' ', @class, ' '), ' FooterBox3 ')]")	
 
 	}
   }
